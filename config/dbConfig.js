0583b1d200d10cc537c83ffd15e04536ac39b57f
sahdaaryanti <nurafni@nurs-MacBook-Air.local>
2023-12-08 10:53:46 +0800

    backend

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,27 +1,26 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'bookish',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
 });
 
 require("dotenv").config();
 const midtransConfig = {
-    clientKey: "SB-Mid-client-1CHFlkgibw5_iOsq",
-    serverKey: "SB-Mid-server-VFMYXvWVa2vxkW9M7RUhgmrh",
-  };
-  
-  pool.connect((err, client, done) => {
-    if (err) {
-      console.error('Error connecting to the database:', err.stack);
-    } else {
-      console.log('Successfully connected to the database');
-      client.release();
-    }
-  });
+  clientKey: "SB-Mid-client-1CHFlkgibw5_iOsq",
+  serverKey: "SB-Mid-server-VFMYXvWVa2vxkW9M7RUhgmrh",
+};
 
-module.exports = {pool, midtransConfig};
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+  } else {
+    console.log("Successfully connected to the database");
+    client.release();
+  }
+});
+
+module.exports = { pool, midtransConfig };
