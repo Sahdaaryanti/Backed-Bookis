8c7b320fa792a7998e80da78a708dcc08a3efdde
sahdaaryanti <nurafni@nurs-MacBook-Air.local>
2023-12-10 13:02:48 +0800

    fixed gudang

--- routes/gudang.js
+++ routes/gudang.js
@@ -10,7 +10,7 @@ const {
 } = require('../controllers/gudang');
 
 // Get all books with stock information
-router.get('/buku', authentication, verifyRole(['admin']), getAllStocks);
+router.get('/buku', authentication, getAllStocks);
 router.post('/buku', authentication, verifyRole(['admin']), addStock);
 router.put('/buku/:gudangId', authentication, verifyRole(['admin']), updateStock);
 router.delete('/buku/:gudangId', authentication, verifyRole(['admin']), deleteStock);
