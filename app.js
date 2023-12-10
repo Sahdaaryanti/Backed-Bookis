8ecdc6fdded41ba4518ec73aebc960167d3a36b3
sahdaaryanti <nurafni@nurs-MacBook-Air.local>
2023-12-10 12:02:18 +0800

    fixed enpoint

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const swaggerUI      = require('swagger-ui-express');
 const routes         = require("./routes/index")
 
 const corsOptions = {
-  origin: ['http://localhost:3001', 'https://frontend-bookis.vercel.app', 'https://frontend-bookish-admin-sahda-aryantis-projects.vercel.app'],
+  origin: ['http://localhost:3001', 'https://frontend-bookis.vercel.app', 'https://frontend-bookish-admin.vercel.app'],
+  credentials: true, 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
